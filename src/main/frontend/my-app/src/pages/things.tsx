2e9b2bef967beea282fb9e26646b51fd97cc8084
EHansonn <evanhanson02@gmail.com>
2023-02-24 16:26:27 -0500

    tailwind for dummy data

--- src/main/frontend/my-app/src/pages/things.tsx
+++ src/main/frontend/my-app/src/pages/things.tsx
@@ -19,14 +19,11 @@ export default function Things() {
   return (
     <h1 className="flex space-y-3 flex-col ">
       {things.map((thing) => (
-        <div className=" bg-red-300" key={thing.id}>
-          Id:{thing.id}
-          <br />
-          Name:{thing.name}
-          <br />
-          Address:{thing.address}
-          <br />
-          Address:{thing.testdata}
+        <div className=" bg-red-300 flex flex-col" key={thing.id}>
+          <div>Id: {thing.id}</div>
+          <div>Name: {thing.name}</div>
+          <div>Address: {thing.address}</div>
+          <div>Testdata: {thing.testdata}</div>
         </div>
       ))}
     </h1>
