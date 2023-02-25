bfc53229bf855a72b89e9b6b7d22bf755bdebd15
EHansonn <evanhanson02@gmail.com>
2023-02-25 13:53:30 -0500

    styling

--- src/main/frontend/my-app/src/components/Navbar.tsx
+++ src/main/frontend/my-app/src/components/Navbar.tsx
@@ -5,17 +5,19 @@ const Navbar = () => {
     <>
       <div className="top-0 w-full left-0 bg-inherit flex justify-start bg-red-900 p-4   ">
         {/* <h1 className="text-lg select-none "> */}
-        <div className="mr-10 font-bold text-white">Title</div>
+        <Link to="/" className="mr-10 no-underline font-bold text-white transition duration-300 hover:scale-105">
+          Title
+        </Link>
         <div className="flex">
           <Link
             to="/"
-            className="text-white mr-10 no-underline transition duration-300 hover:scale-125"
+            className="text-white mr-10 no-underline transition duration-300 hover:scale-105"
           >
             Home
           </Link>
           <Link
             to="/things"
-            className="text-white mr-10 no-underline transition duration-300 hover:scale-125"
+            className="text-white mr-10 no-underline transition duration-300 hover:scale-105"
           >
             Things
           </Link>
